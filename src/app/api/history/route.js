77a5737261ef2c8ddcc0df0ac5aa7308ae996a40
Mehdi Ali <mehdikhanofficial@gmail.com>
2024-09-19 23:42:55 +0500

    updated

--- src/app/api/history/route.js
+++ src/app/api/history/route.js
@@ -8,6 +8,7 @@ export const GET = async (req) => {
 
     try {
         const resumes = await Resume.find();
+        console.log('resumes from mongo: ', resumes);
         return NextResponse.json( resumes , { status: 200 });
     } catch (err) {
         console.error('API route error:', err);
