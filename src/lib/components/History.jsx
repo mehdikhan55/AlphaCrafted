eabe2d91a07f62ed1fb4d349826850c1edc65518
Mehdi Ali <mehdikhanofficial@gmail.com>
2024-09-20 13:46:31 +0500

    fixed history issue

--- src/lib/components/History.jsx
+++ src/lib/components/History.jsx
@@ -4,65 +4,68 @@ import React, { useEffect, useState } from 'react'
 import toast from 'react-hot-toast'
 
 const History = () => {
-    const[resumes, setResumes] = useState([])
-    const[loading, setLoading] = useState(true)
+    const [resumes, setResumes] = useState([])
+    const [loading, setLoading] = useState(true)
 
     useEffect(() => {
         const fetchResumes = async () => {
             try {
                 const res = await fetch('/api/history', { cache: "no-store" });  // Disable caching here
                 const data = await res.json()
-            
+                // console.log('fetched data: ', data);
                 setResumes(data);  // Set the resumes state
-                
-               
-            }catch (error) {
+                // console.log('resuems fetched: ', resumes);
+            } catch (error) {
                 console.log(error)
-                toast.error('Failed to fetch resumes')
-            }finally {
-                setLoading(false)
+                toast.error('Failed to fetch resumes');
+                setLoading(false);
             }
-            
         }
         fetchResumes();
     }, []);
 
-   
+
+    useEffect(() => {
+        if (resumes.length > 0) {
+            setLoading(false);
+            // console.log('second use effect resumes: ', resumes);
+        }
+    }, [resumes]);
 
 
     return (
         <>
-        {
-            loading ? (
-                // make a loader here
-                <div className="flex justify-center items-center h-screen">
-                    <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-slate-500"></div>
-                </div>
+            {
+                loading ? (
+                    // make a loader here
+                    <div className="flex justify-center items-center h-screen">
+                        <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-slate-500"></div>
+                    </div>
 
-            )
-            :
-            (
-                <div className="py-5 border-t-2 border-t-slate-300 max-w-7xl pt-4 mx-auto grid grid-cols-2 items-start lg:grid-cols-3 px-5">
-                {resumes.map((resume) => {
-                    //make a card
-                    return (
-                        <Link key={resume._id} href={`/resume-preview/${resume._id}`} target="_blank" >
-                            <div  className="bg-white shadow-xl rounded-lg mx-4 my-4 border border-slate-200">
-                                <div className="h-40 overflow-hidden">
-                                </div>
-                                <div className="p-6">
-                                    <h2 className="text-xl font-bold text-gray-800">{resume.fullName}</h2>
-                                    <p className="leading-relaxed mb-3">{resume.fullName}</p>
-                                </div>
-                            </div>
-                        </Link>
+                )
+                    :
+                    (
+                        <div className="py-5 border-t-2 border-t-slate-300 max-w-7xl pt-4 mx-auto grid grid-cols-2 items-start lg:grid-cols-3 px-5">
+                            {resumes.map((resume) => {
+                                //make a card
+                                return (
+                                    <Link key={resume._id} href={`/resume-preview/${resume._id}`} target="_blank" >
+                                        <div className="bg-white shadow-xl rounded-lg mx-4 my-4 border border-slate-200">
+                                            <div className="h-40 overflow-hidden">
+                                            </div>
+                                            <div className="p-6">
+                                                <h2 className="text-xl font-bold text-gray-800">{resume.fullName}</h2>
+                                                <p className="leading-relaxed mb-3">{resume.fullName}</p>
+                                            </div>
+                                        </div>
+                                    </Link>
+                                )
+                            })}
+                        </div>
                     )
-                })}
-            </div>
-            )
-        }
+            }
         </>
-       
+
     )
 }
 
